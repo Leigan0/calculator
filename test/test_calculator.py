ceb3f245e1c28f502d943ab04a4dda1f315b4eeb
Leigh-ann <leighann.gant@gmail.com>
2018-03-21 13:33:48 +0000

    Value error raised if non number passed in

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -2,7 +2,17 @@ import unittest
 from app.calculator import Calculator
 
 class TddInPythonAttempt(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
     def test_calculator_add_method_returns_correct_result(self):
-        calc = Calculator()
-        result = calc.add(2,2)
+        result = self.calc.add(2,2)
         self.assertEqual(4, result)
+
+    def test_calculator_add_method_returns_correct_results(self):
+        result = self.calc.add(10,2)
+        self.assertEqual(12, result)
+
+    def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+        self.assertRaises(ValueError, self.calc.add, 'two', 'three')
