8c59ad720a2753c7ac46a816ead6375ebfe93ff5
Leigh-ann <leighann.gant@gmail.com>
2018-03-21 14:22:22 +0000

    Validate input refactored

--- app/calculator.py
+++ app/calculator.py
@@ -1,3 +1,13 @@
 class Calculator(object):
+
+    def validate_input(self, x, y):
+        number_types = (int, long, float, complex)
+        return isinstance(x, number_types) and isinstance(y, number_types)
+
     def add(self, x, y):
-        return x + y 
+        if self.validate_input(x, y): return x + y
+        else: raise ValueError
+
+    def subtract(self, x, y):
+        if self.validate_input(x, y): return x - y
+        else: raise ValueError
